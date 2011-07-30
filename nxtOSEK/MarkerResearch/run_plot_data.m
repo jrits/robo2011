04f156a6266e5d84c13d6440e23151554b2df823
Naotoshi Seo <sonots@gmail.com>
2011-07-30 17:54:56 +0900

    Removed robo2009, robo2010, samples

--- nxtOSEK/MarkerResearch/run_plot_data.m
+++
@@ -1,9 +0,0 @@
-## run_plot_data
-
-## Author: nseo <nseo@NSEO>
-## Created: 2009-07-22
-
-daq = read_light_daq('light_daq/daq.csv');
-
-figure; plot(daq); ylim([500, 750]);
-print -deps 'daq.eps'; print -djpg 'daq.jpg';
