2a0dd705f84ad9a3f28618af02c3dd340f5ba06f
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2011-08-03 13:58:10 +0900

    Move implemenation from header file to cpp file.

--- nxtOSEK/etrobo2011/SitDownSkill.cpp
+++ nxtOSEK/etrobo2011/SitDownSkill.cpp
@@ -1,1 +1,56 @@
 #include "SitDownSkill.h"
+
+extern "C" {
+#include "balancer.h"
+}
+
+#include "Motor.h"
+#include "Nxt.h"
+#include "GyroSensor.h"
+#include "constants.h"
+#include "Vector.h"
+
+using namespace ecrobot;
+
+extern Motor mLeftMotor;
+extern Motor mRightMotor;
+extern Motor mTailMotor;
+extern Nxt mNxt;
+extern GyroSensor mGyroSensor;
+
+void SitDownSkill::execute(){
+  S8 pwmL = 0, pwmR=0;
+
+  VectorT<float> command(0.0,0.0);
+  balance_control((F32)command.mX, // 前後進命令
+                  (F32)command.mY,
+                  (F32)mGyroSensor.get(),
+                  (F32)USER_GYRO_OFFSET,
+                  (F32)mLeftMotor.getCount(),
+                  (F32)mRightMotor.getCount(),
+                  (F32)mNxt.getBattMv(),
+                  &pwmL,
+                  &pwmR);
+  tail_control(100);
+  mLeftMotor.setPWM(pwmL + 10);
+  mRightMotor.setPWM(pwmR + 10);
+}
+
+void
+SitDownSkill::tail_control(signed int angle) const{
+  static const float P_GAIN = 2.5F;
+  static const float PWM_ABS_MAX = 60; /* 完全停止用モータ制御PWM絶対最大値 */        
+  float pwm = (float)(angle - mTailMotor.getCount())*P_GAIN; /* 比例制御 */
+
+  /* PWM出力飽和処理 */
+  if (pwm > PWM_ABS_MAX)
+  {
+    pwm = PWM_ABS_MAX;
+  }
+  else if (pwm < -PWM_ABS_MAX)
+  {
+    pwm = -PWM_ABS_MAX;
+  }
+  mTailMotor.setPWM((S8)pwm);
+  mTailMotor.setBrake(true);
+}
