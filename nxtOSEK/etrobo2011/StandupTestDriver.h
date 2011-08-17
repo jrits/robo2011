a62cdb0f800538b0eac9556281c856c9fa645256
kachirobo <kachirobo@prnhost-wide2.(none)>
2011-08-17 21:38:14 +0900

    added StandupTestDriver.

---
+++ nxtOSEK/etrobo2011/StandupTestDriver.h
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////
+//  TestDriver.h
+//  Definition of the Class TestDriver
+//  Created on:      10-9-2010 19:43:43
+///////////////////////////////////////////////////////////
+#ifndef TESTDRIVER_H_
+#define TESTDRIVER_H_
+#define P_GAIN             2.5F /* 完全停止用モータ制御比例係数 */
+#define PWM_ABS_MAX          60 /* 完全停止用モータ制御PWM絶対最大値 */
+
+
+#include "Driver.h"
+
+/**
+ * テストドライバ
+ */
+class TestDriver : public Driver
+{ 
+public:
+		enum eSubSection {
+        INIT = -1,
+        TAILPUSH,
+        STOPTAILPUSH
+    };
+	TestDriver();
+	~TestDriver(){}
+    bool drive();
+//	void mTail_control(signed int angle);
+	int mState;
+	int SSC;//State Switch Counter
+	float beforeRMH; //状態変化時のモータ回転角度（積算値）
+	signed char mPwm_L;
+	signed char mPwm_R;
+};
+
+#endif
