bb41f51c7401dfe60a7a9a3fc3a8736013c126f6
U-Mikio-PC\Mikio <Mikio@Mikio-PC.(none)>
2011-09-18 14:58:57 +0900

    アウトコースも同じ動きでできました

--- nxtOSEK/etrobo2011/NormalDriver.cpp
+++ nxtOSEK/etrobo2011/NormalDriver.cpp
@@ -76,9 +76,55 @@ bool NormalDriver::drive()
     	mForwardPid.reset(0.003, 0, 0.01);
     	mLineTrace.setForward(100);
     }
+
 #else
-#endif
+	// スタートから坂道頂上まで
+    if(0 < mGps.getDistance() && mGps.getDistance() < 1500.0) {
+        mLightPid.reset(30,0,90);
+        mForwardPid.reset(0.003, 0, 0.01);
+        mLineTrace.setForward(150);
+    }
 	
+	// 坂道頂上直前から坂道頂上直後まで
+	else if(1500.0 < mGps.getDistance() && mGps.getDistance() < 1600.0) {
+        mLightPid.reset(30,0,90);
+        mForwardPid.reset(0.003, 0, 0.01);
+        mLineTrace.setForward(90);
+    }
+	
+	// 坂道頂上直後から下り坂中腹まで
+	else if(1600.0 < mGps.getDistance() && mGps.getDistance() < 1800.0) {
+        mLightPid.reset(30,0,90);
+        mForwardPid.reset(0.3, 0, 0.9);
+		mLineTrace.setForward(60);
+    }
+	// 下り坂中腹から第1コーナーの入り口まで
+	else if(1800.0 < mGps.getDistance() && mGps.getDistance() < 2100.0) {
+        mLightPid.reset(30,0,90);
+        mForwardPid.reset(0.1, 0, 0.3);
+		mLineTrace.setForward(90);
+    }
+	// 第1コーナーの入り口から第1コーナー出口まで
+    else if(2100.0 < mGps.getDistance() && mGps.getDistance() < 4100.0) {
+        mLightPid.reset(70,0,210);
+    	mForwardPid.reset(0.003, 0, 0.01);
+    	mLineTrace.setForward(100);
+    }
+	
+	// 第1コーナーの出口から第2コーナーの入り口まで
+    else if(4100.0 < mGps.getDistance() && mGps.getDistance() < 5200.0) {
+        mLightPid.reset(70,0,210);
+    	mForwardPid.reset(0.003, 0, 0.01);
+    	mLineTrace.setForward(120);
+    }	
+	// 第2コーナーの入り口から最後まで
+    else if(5200.0 < mGps.getDistance()) {
+        mLightPid.reset(100,0,300);
+    	mForwardPid.reset(0.003, 0, 0.01);
+    	mLineTrace.setForward(100);
+    }
+
+#endif	
     mLineTrace.execute();
     return true;
 }
