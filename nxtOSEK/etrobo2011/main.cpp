9ed5ec41156fc9393764c283f8d61252b22dcd59
Kenta Takahashi <user@user-PC.(none)>
2011-09-22 22:52:06 +0900

    LOG ON

--- nxtOSEK/etrobo2011/main.cpp
+++ nxtOSEK/etrobo2011/main.cpp
@@ -177,7 +177,7 @@ TASK(TaskDrive)
     WaitEvent(EventDrive);
     // K_THETADOT = 10.5F;
 
-    //connect_bt(mLcd, BT_NAME); // bluetooth接続
+    connect_bt(mLcd, BT_NAME); // bluetooth接続
     mActivator.reset(USER_GYRO_OFFSET);
     gDoMaimai = false;
 
